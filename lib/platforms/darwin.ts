594f15c70fab16c02bad18efba8ac20e1b84447a
John Vilk <jvilk@cs.umass.edu>
2015-10-07 16:04:57 -0400

    Using *all* listed JVMs for Darwin.

--- lib/platforms/darwin.ts
+++ lib/platforms/darwin.ts
@@ -11,15 +11,20 @@ var exec = child_process.exec;
  */
 export = function macFindJavaHome(cb: (homes: string[], executableExtension?: string) => void): void {
   var discoveredJavaHomes: string[] = [];
-  // TODO: Look through entire list.
-  async.eachSeries(['5','6','7','8','9'], (version: string, asyncCb: (err?: Error) => void) => {
-    exec('/usr/libexec/java_home -version 1.' + version, (err: Error, stdout: Buffer, stderr: Buffer) => {
-      if (!err) {
-        discoveredJavaHomes.push(stdout.toString().replace('\n', '').trim());
-      }
-      asyncCb();
-    });
-  }, (err?: Error) => {
-    cb(discoveredJavaHomes);
+  exec('/usr/libexec/java_home -V', (err: Error, stdout: Buffer, stderr: Buffer) => {
+    /*
+      Output example, which java_home prints to stderr [!]:
+      Matching Java Virtual Machines (4):
+        1.8.0_60, x86_64:	"Java SE 8"	/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home
+        1.7.0_79, x86_64:	"Java SE 7"	/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home
+        1.6.0_65-b14-468, x86_64:	"Java SE 6"	/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+        1.6.0_65-b14-468, i386:	"Java SE 6"	/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+     */
+    // Ditch boilerplate first line, and trim ending newlines.
+    var installations = stderr.toString().trim().split('\n').slice(1);
+    // Map to paths.
+    // TODO: We assume that quotes cannot be in the paths.
+    installations = installations.map((install) => install.slice(install.lastIndexOf('"') + 1).trim());
+    cb(installations);
   });
 }
